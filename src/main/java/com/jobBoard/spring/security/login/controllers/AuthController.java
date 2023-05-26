850d193db8ec437eab2303f37e6124b3b034c6b8
Rajesh Gottimukkula <rajesh.gottimukkula1@gmail.com>
2023-05-26 12:29:15 +0530

    Added recruiter signin and signup

--- src/main/java/com/jobBoard/spring/security/login/controllers/AuthController.java
+++ src/main/java/com/jobBoard/spring/security/login/controllers/AuthController.java
@@ -102,6 +102,11 @@ public class AuthController {
 
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+	  
+	  
+	  
+	  
+	  
     if (userRepository.existsByUsername(signUpRequest.getUsername())) {
       return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
     }
